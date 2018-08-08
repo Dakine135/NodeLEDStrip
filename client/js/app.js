aba1b5e09b924d35bc817166c457493c06408363
Dustin Welborn <Dustinater@gmail.com>
2018-08-08 17:10:49 -0400

    working on tilt

--- client/js/app.js
+++ client/js/app.js
@@ -189,35 +189,116 @@ var mainApp = new Vue({
 
 socket.on('clientUpdate', function(data){
   console.log("clientUpdate: ", data);
-  mainApp.currentState = data.stateName;
+  // mainApp.currentState = data.stateName;
   mainApp.speed = data.settings.speed;
   mainApp.colors = data.settings.colors;
 });
 
 
 
-if (window.DeviceMotionEvent == undefined) {
-    //No accelerometer is present. Use buttons.
-    // alert("no accelerometer");
-} else {
-    // alert("accelerometer found");
-    window.addEventListener("devicemotion", accelerometerUpdate, true);
-}
+
+
 
 var count = 0;
-function accelerometerUpdate(e) {
-   var aX = event.accelerationIncludingGravity.x*1;
-   var aY = event.accelerationIncludingGravity.y*1;
-   var aZ = event.accelerationIncludingGravity.z*1;
-   //The following two lines are just to calculate a
-   // tilt. Not really needed.
-   xPosition = Math.atan2(aY, aZ);
-   yPosition = Math.atan2(aX, aZ);
-   data = {x:xPosition, y:yPosition};
-   // console.log(data);
-   // alert("moveEvent: ",data);
-   if(count > 100){
-     socket.emit("tilt", data);
-     count = 0;
-   } else count++;
-}
+var data = {};
+
+window.onload = function(){
+
+  if (window.DeviceMotionEvent == undefined) {
+      //No accelerometer is present. Use buttons.
+      // alert("no accelerometer");
+  } else {
+      // alert("accelerometer found");
+      // window.addEventListener("devicemotion", accelerometerUpdate, true);
+      window.addEventListener('deviceorientation', handleOrientation);
+  }
+
+
+  console.log(document);
+  console.log(document.querySelector('.garden'));
+  var ball   = document.querySelector('.ball');
+  var garden = document.querySelector('.garden');
+  var output = document.querySelector('.output');
+
+  var maxX = garden.clientWidth  - ball.clientWidth;
+  var maxY = garden.clientHeight - ball.clientHeight;
+
+  function handleOrientation(event){
+    var x = event.beta;  // In degree in the range [-180,180]
+    var y = event.gamma; // In degree in the range [-90,90]
+
+    output.innerHTML  = "beta : " + x + "\n";
+    output.innerHTML += "gamma: " + y + "\n";
+
+    // Because we don't want to have the device upside down
+    // We constrain the x value to the range [-90,90]
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
+
+    // To make computation easier we shift the range of
+    // x and y to [0,180]
+    x += 90;
+    y += 90;
+
+    // 10 is half the size of the ball
+    // It center the positioning point to the center of the ball
+    ball.style.top  = (maxX*x/180 - 10) + "px";
+    ball.style.left = (maxY*y/180 - 10) + "px";
+
+    let roundX = Math.round( x);
+    let roundY = Math.round( y);
+
+    data = {x:roundX, y:roundY};
+
+    // if (data.x == null || data.x == undefined){
+    //      data = {x:x, y:y};
+    //    } else {
+    //      let avgX = (0.7 * x) + (0.3 * data.x);
+    //      let avgY = (0.7 * y) + (0.3 * data.y);
+    //      let roundAvgX = (Math.round( avgX * 100 )) / 100;
+    //      let roundAvgY = (Math.round( avgY * 100 )) / 100;
+    //      data = {x:roundAvgX, y:roundAvgY};
+    //    }
+
+       // console.log(data);
+       // alert("moveEvent: ",data);
+       if(count > 20){
+         socket.emit("tilt", data);
+         count = 0;
+       } else count++;
+  }
+}//windows onload
+
+
+
+
+//
+// var count = 0;
+// var data = {};
+// function accelerometerUpdate(e) {
+//    var aX = event.accelerationIncludingGravity.x*1;
+//    var aY = event.accelerationIncludingGravity.y*1;
+//    var aZ = event.accelerationIncludingGravity.z*1;
+//    let dataRaw = {aX: aX, aY: aY, aZ: aZ};
+//    //The following two lines are just to calculate a
+//    // tilt. Not really needed.
+//    xPosition = Math.atan2(aY, aZ);
+//    yPosition = Math.atan2(aX, aZ);
+//    if (data.x == null || data.x == undefined){
+//      data = {x:xPosition, y:yPosition};
+//    } else {
+//      let avgX = (0.7 * xPosition) + (0.3 * data.x);
+//      let avgY = (0.7 * yPosition) + (0.3 * data.y);
+//      let roundAvgX = (Math.round( avgX * 100 )) / 100;
+//      let roundAvgY = (Math.round( avgY * 100 )) / 100;
+//      data = {x:roundAvgX, y:roundAvgY};
+//    }
+//
+//    // console.log(data);
+//    // alert("moveEvent: ",data);
+//    if(count > 50){
+//      socket.emit("tilt", data);
+//      socket.emit("tilt", dataRaw);
+//      count = 0;
+//    } else count++;
+// }
