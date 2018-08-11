c06419af387ad7de9ec7ed7d8025abd8dc0bdfb3
welbordn <welbordn@email.sc.edu>
2018-08-11 11:32:25 -0400

    added client pulses

--- states/Pulse.js
+++ states/Pulse.js
@@ -12,29 +12,51 @@ class Pulse{
   update(delta){
 
     //clear LEDS and set the end to what color it needs to be
-    var i=this.end;
+    var tempEnd=this.end;
     for (var i=0; i < this.strip.totalLeds; i++)
     {
         if(i >= this.end) this.strip.pixelData[i] = this.strip.colorwheel((this.colorPos + i) % 256);
         else this.strip.pixelData[i] = 0;
     }
 
-    //set color of LED that the AutoPulse is on
+    //draw autopulse
     let indexFloor = Math.floor(this.autoPulseIndex);
-    this.strip.pixelData[indexFloor] = this.strip.colorwheel((this.colorPos + i) % 256);
-
-    for(var i=0; i < this.pulses.length; i++){
-
-    }
-
+    this.strip.pixelData[indexFloor] = this.strip.colorwheel((this.colorPos + tempEnd) % 256);
+    //move the autopulse down the strip every update
+    this.autoPulseIndex = this.autoPulseIndex + (delta * this.strip.stateSettings.speed);
     //if autopulse reaches the end, then reset to begining and increment the end
-    if((this.autoPulseIndex + 1) >= this.end){
+    if(this.autoPulseIndex >= this.end){
       this.autoPulseIndex = 0;
       this.end--;
     }
 
-    //move the autopulse down the strip every update
-    this.autoPulseIndex = (this.autoPulseIndex + (delta * this.strip.stateSettings.speed));
+    //remove finished client Pulses signified by index -1
+    var deleteIndex = 0;
+    while(deleteIndex < this.pulses.length){
+      if(this.pulses[deleteIndex].index == -1){
+        this.pulses.splice(deleteIndex, 1);
+      } else {
+        deleteIndex++;
+      }
+    }
+
+    //client Pulses
+    for(var i=0; i < this.pulses.length; i++){
+      //draw at index
+      let index = Math.floor(this.pulses[i].index);
+      this.strip.pixelData[indexFloor] = this.pulses[i].color;
+      //increment based in its speed
+      this.pulses[i].index = this.pulses[i].index + (delta * this.pulses[i].speed);
+      //test for End
+      if(this.pulses[i].index >= this.end){
+        this.pulses[i].index = -1;
+        this.end--;
+      }
+    }
+
+
+
+
 
     //if the entire thing fills up, then reset the end
     if(this.end == 0){
